265c507dfd77e321e0eed71f4cca256e24900f07
benimaru73 <fitahinaleong@gmail.com>
2025-12-30 12:08:25 +0300

    [Ordering]

---
+++ js/experience.js
@@ -0,0 +1,28 @@
+const experienceData = [
+    {
+        "title": "Développeur Full Stack Junior",
+        "company": "Bici",
+        "companyColorClass": "text-blue-600 dark:text-blue-400",
+        "period": "Decembre 2025 - Present",
+        "description": "Développement et maintenance d'une application de gestion comptable pour les entreprises. Contribution à l'amélioration des fonctionnalités existantes et à l'implémentation de nouvelles fonctionnalités en utilisant Java EE.",
+        "tags": [
+            { "name": "JEE", "bgClass": "bg-blue-100 dark:bg-blue-900", "textClass": "text-blue-800 dark:text-blue-200" },
+            { "name": "Full Stack", "bgClass": "bg-blue-100 dark:bg-blue-900", "textClass": "text-blue-800 dark:text-blue-200" }
+        ],
+        "borderClass": "bg-blue-500"
+    },
+    {
+        "title": "Stagiaire Full Stack",
+        "company": "Ibonia",
+        "companyColorClass": "text-teal-600 dark:text-teal-400",
+        "period": "Mai 2025 - Octobre 2025",
+        "description": "Plateforme de conception d'USSD pour un Entité. Conception et développement d'une plateforme USSD générique permettant aux entreprises de créer et gérer des services USSD dynamiques via une interface web conviviale. Utilisation de Java Spring Boot pour le backend et Next.js pour le frontend. Avec un IDE graphique.",
+        "tags": [
+            { "name": "Java Spring Boot", "bgClass": "bg-teal-100 dark:bg-teal-900", "textClass": "text-teal-800 dark:text-teal-200" },
+            { "name": "Next.js", "bgClass": "bg-teal-100 dark:bg-teal-900", "textClass": "text-teal-800 dark:text-teal-200" },
+            { "name": "USSD", "bgClass": "bg-teal-100 dark:bg-teal-900", "textClass": "text-teal-800 dark:text-teal-200" }
+        ],
+        "borderClass": "bg-teal-500"
+    }
+];
+
